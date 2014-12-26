3c9eadd6239ad78620651a49765037dc34de3633
taukir4u <taukir4u@gmail.com>
2014-12-26 20:10:37 +0600

    C#
    Signed-off-by: taukir4u <taukir4u@gmail.com>

---
+++ InterestCalculator/InterestCalculatorUI.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace InterestCalculator
+{
+    partial class InterestCalculatorUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.timeTextBox = new System.Windows.Forms.TextBox();
+            this.balanceTextBox = new System.Windows.Forms.TextBox();
+            this.bankComboBox = new System.Windows.Forms.ComboBox();
+            this.calculateInterestButton = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.interestShowlabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Bank Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Time In Year";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 142);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Balance";
+            // 
+            // timeTextBox
+            // 
+            this.timeTextBox.Location = new System.Drawing.Point(174, 64);
+            this.timeTextBox.Name = "timeTextBox";
+            this.timeTextBox.Size = new System.Drawing.Size(100, 20);
+            this.timeTextBox.TabIndex = 3;
+            // 
+            // balanceTextBox
+            // 
+            this.balanceTextBox.Location = new System.Drawing.Point(174, 135);
+            this.balanceTextBox.Name = "balanceTextBox";
+            this.balanceTextBox.Size = new System.Drawing.Size(100, 20);
+            this.balanceTextBox.TabIndex = 4;
+            // 
+            // bankComboBox
+            // 
+            this.bankComboBox.FormattingEnabled = true;
+            this.bankComboBox.Items.AddRange(new object[] {
+            "BRAC",
+            "DBBL",
+            "HSBC"});
+            this.bankComboBox.Location = new System.Drawing.Point(174, 6);
+            this.bankComboBox.Name = "bankComboBox";
+            this.bankComboBox.Size = new System.Drawing.Size(121, 21);
+            this.bankComboBox.TabIndex = 5;
+            // 
+            // calculateInterestButton
+            // 
+            this.calculateInterestButton.Location = new System.Drawing.Point(174, 194);
+            this.calculateInterestButton.Name = "calculateInterestButton";
+            this.calculateInterestButton.Size = new System.Drawing.Size(123, 23);
+            this.calculateInterestButton.TabIndex = 6;
+            this.calculateInterestButton.Text = "Calculate Interest";
+            this.calculateInterestButton.UseVisualStyleBackColor = true;
+            this.calculateInterestButton.Click += new System.EventHandler(this.calculateInterestButton_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(171, 247);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Interest=";
+            // 
+            // interestShowlabel
+            // 
+            this.interestShowlabel.AutoSize = true;
+            this.interestShowlabel.Location = new System.Drawing.Point(239, 247);
+            this.interestShowlabel.Name = "interestShowlabel";
+            this.interestShowlabel.Size = new System.Drawing.Size(0, 13);
+            this.interestShowlabel.TabIndex = 8;
+            // 
+            // InterestCalculatorUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(572, 332);
+            this.Controls.Add(this.interestShowlabel);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.calculateInterestButton);
+            this.Controls.Add(this.bankComboBox);
+            this.Controls.Add(this.balanceTextBox);
+            this.Controls.Add(this.timeTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "InterestCalculatorUI";
+            this.Text = "InterestCalculatorUI";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox timeTextBox;
+        private System.Windows.Forms.TextBox balanceTextBox;
+        private System.Windows.Forms.ComboBox bankComboBox;
+        private System.Windows.Forms.Button calculateInterestButton;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label interestShowlabel;
+    }
+}
+
---
+++ InterestCalculator/InterestCalculatorUI.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InterestCalculator
+{
+    public partial class InterestCalculatorUI : Form
+    {
+        public InterestCalculatorUI()
+        {
+            InitializeComponent();
+        }
+
+        private string bankName;
+        private double time;
+        private double balance;
+        private double interest;
+
+        public void TakeInput()
+        {
+            bankName = bankComboBox.Text;
+            time = Convert.ToDouble(timeTextBox.Text);
+            balance = Convert.ToDouble(balanceTextBox.Text);
+        }
+        
+        private void calculateInterestButton_Click(object sender, EventArgs e)
+        {
+            TakeInput();
+            if (bankName == "BRAC")
+            {
+                interest = (balance*6*time)/100;
+                interestShowlabel.Text = interest.ToString();
+            }
+            else if (bankName == "DBBL")
+            {
+                interest = (balance * 7 * time) / 100;
+                interestShowlabel.Text = interest.ToString();
+            }
+            else if (bankName == "HSBC")
+            
+            {
+                interest = (balance * 8 * time) / 100;
+                interestShowlabel.Text = interest.ToString();
+            }
+            else
+            {
+                MessageBox.Show("please select a bank name");
+            }
+
+        }
+    }
+}
